eb116d207039e737af1c0180d10ac82d8b079330
Margit Robert <marrob50hz@gmail.com>
2021-07-13 09:14:39 +0200

    update

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -5,6 +5,29 @@ import { LoadingController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
 import { PlaceLocation } from '../location.model';
 
+
+function base64toBlob(base64Data, contentType) {
+  contentType = contentType || '';
+  const sliceSize = 1024;
+  const byteCharacters = window.atob(base64Data);
+  const bytesLength = byteCharacters.length;
+  const slicesCount = Math.ceil(bytesLength / sliceSize);
+  const byteArrays = new Array(slicesCount);
+
+  for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+    const begin = sliceIndex * sliceSize;
+    const end = Math.min(begin + sliceSize, bytesLength);
+
+    const bytes = new Array(end - begin);
+    for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+      bytes[i] = byteCharacters[offset].charCodeAt(0);
+    }
+    byteArrays[sliceIndex] = new Uint8Array(bytes);
+  }
+  return new Blob(byteArrays, { type: contentType });
+}
+
+
 @Component({
   selector: 'app-new-offer',
   templateUrl: './new-offer.page.html',
@@ -41,7 +64,8 @@ export class NewOfferPage implements OnInit {
         updateOn:'blur',
         validators:[Validators.required]
       }),
-      location: new FormControl(null, {validators: [Validators.required]})
+      location: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null)
     });
   }
 
@@ -75,7 +99,23 @@ export class NewOfferPage implements OnInit {
     });
   }
 
-  onImagePicked(imageData:string){
+  onImagePicked(imageData:string | File){
+
+    
     console.log("image data" + imageData);
+    
+    let imageFile;
+
+    if(typeof imageData =='string'){
+      try{
+        imageFile = base64toBlob(imageData.replace('data:image/jpeg;base64',''),'image/jpeg');
+      }catch(error){
+        console.log(error);
+        return;
+      }
+    }else{
+      imageFile = imageData;
+    }
+    this.form.patchValue({image:imageFile});
   }
 }

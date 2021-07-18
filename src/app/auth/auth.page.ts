a9364f7d63738bfd97ab4826dcb874a5968d632e
Margit Robert <marrob50hz@gmail.com>
2021-07-18 22:21:46 +0200

    update

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { NgForm, } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
+
 
 
 @Component({
@@ -24,7 +25,8 @@ export class AuthPage implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private loadingCtrl: LoadingController) {
+    private loadingCtrl: LoadingController,
+    private alertCtrl:AlertController) {
   }
   ngOnInit() {
   }
@@ -37,34 +39,44 @@ export class AuthPage implements OnInit {
     }
     const email = form.value.email;
     const password = form.value.password;
-    console.log("email-password", email, password);
-    this.onLogin();
-    if (this.isLogin) {
-
-    }
-    else {
-      this.authService.singupV2(email, password).subscribe(resData=>{
-        console.log(resData);
-      })
-    }
+    this.authenticate(email, password);
   }
 
   onSwitchAuthMode(){
     this.isLogin =!this.isLogin;
   }
 
-  onLogin() { 
+  authenticate(email:string, password:string) { 
     this.isLaoding = true;
     this.authService.login();
     this.loadingCtrl.create({ keyboardClose: true, message: 'Logging in...' })
       .then(loadingEl => {
         loadingEl.present();   
-        setTimeout(() => {
+        this.authService.singupV2(email, password).subscribe(resData=>{
+          console.log(resData);       
           this.isLaoding = false;
           loadingEl.dismiss();
           this.router.navigateByUrl('/places/tabs/discover');
-        }, 1500);
+        }, errResp =>{
+           this.isLaoding = false;
+           loadingEl.dismiss();
+           const code = errResp.error.error.message;
+           let message ='Could not sign you, please try agian';
+           if(code==='EMAIL_EXISTS'){
+             message = 'This email address already exists!'
+           }
+           this.showAlert(message);
+        })
       });
+  }
+
+  private showAlert(message:string){
+    this.alertCtrl
+    .create({
+      header:'Authetication failed', 
+      message:message,
+      buttons:['Okay']
+      }).then(alertEl=>alertEl.present());
   }
 
 }
